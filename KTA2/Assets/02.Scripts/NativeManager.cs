a085fbfad1cc1655d20bc3f8d97b58e858b46b0d
SLINNOVATIONS\ssmktr <ssmktr@gmail.com>
2018-04-10 17:05:35 +0900

    no message

--- KTA2/Assets/02.Scripts/NativeManager.cs
+++ KTA2/Assets/02.Scripts/NativeManager.cs
@@ -9,11 +9,14 @@ public class NativeManager : Singleton<NativeManager>
 {
     public void Init()
     {
-        FB.Init(FaceBookInit, OnHideUnity);
+        FaceBookInit();
     }
 
     #region FACEBOOK
     System.Action FaceBookLoginCallBack = null;
+    System.Action<string> GetFaceBookFirstNameResult = null;
+    System.Action<Sprite> GetFaceBookProfileResult = null;
+
     public void FaceBookLogin(System.Action faceBookLoginCallBack)
     {
         FaceBookLoginCallBack = faceBookLoginCallBack;
@@ -22,13 +25,22 @@ public class NativeManager : Singleton<NativeManager>
 
     void FaceBookInit()
     {
-        if (FB.IsLoggedIn)
+        if (FB.IsInitialized)
         {
-
+            FB.ActivateApp();
         }
         else
         {
+            FB.Init(FaceBookInitCallBack, OnHideUnity);
         }
+    }
+
+    void FaceBookInitCallBack()
+    {
+        if (FB.IsInitialized)
+            FB.ActivateApp();
+        else
+            Debug.Log("Failed to Initialize the Facebook SDK");
     }
 
     void OnHideUnity(bool isGameShown)
@@ -39,14 +51,64 @@ public class NativeManager : Singleton<NativeManager>
             Time.timeScale = 0;
     }
 
-    void DealWithFBMenus(bool isLoggedIn)
+    public void FaceBookLogin()
     {
-        if (isLoggedIn)
+        List<string> perms = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(perms, AuthCllBack);
+    }
+
+    void AuthCllBack(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
         {
+            AccessToken aToken = AccessToken.CurrentAccessToken;
+
+            Debug.Log(aToken.UserId);
+            foreach (string perm in aToken.Permissions)
+                Debug.Log(perm);
         }
         else
+            Debug.Log("User cancelled login");
+    }
+
+    public void GetProfile(System.Action<Sprite> getFaceBookProfileResult)
+    {
+        GetFaceBookProfileResult = getFaceBookProfileResult;
+
+        FB.API("/me/picture?type=square&height=128&width=128", HttpMethod.GET, DisplayProfilePic);
+    }
+
+    void DisplayProfilePic(IGraphResult result)
+    {
+        if (result.Error != null)
         {
+            Debug.Log("Problem with getting user profile");
+            return;
         }
+
+        Sprite profile = Sprite.Create(result.Texture, new Rect(0, 0, 128, 128), new Vector2());
+        if (GetFaceBookProfileResult != null)
+            GetFaceBookProfileResult(profile);
+        GetFaceBookProfileResult = null;
+    }
+
+    public void GetUserFirstName(System.Action<string> getFaceBookFirstNameResult)
+    {
+        GetFaceBookFirstNameResult = getFaceBookFirstNameResult;
+        FB.API("/me?fields=id, first_name", Facebook.Unity.HttpMethod.GET, DealWithUserName);
+    }
+
+    void DealWithUserName(IResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log("Problem with getting user name");
+            return;
+        }
+
+        if (GetFaceBookFirstNameResult != null)
+            GetFaceBookFirstNameResult("UserFirstName");
+        GetFaceBookFirstNameResult = null;
     }
 
     #endregion // FACEBOOK
